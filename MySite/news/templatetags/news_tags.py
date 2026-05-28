8db9e29073e152ab58ddd2a961359aed6645ec2c
seve17nteen <raydenonelove@gmail.com>
2026-05-28 03:25:24 +0300

    9 лабораторная

---
+++ MySite/news/templatetags/news_tags.py
@@ -0,0 +1,16 @@
+print("news_tags.py загружен!")
+
+
+from django import template
+from news.models import Category
+register=template.Library()
+@register.simple_tag(name='get_list_categories')
+
+def get_categories():
+    return Category.objects.all()
+
+@register.inclusion_tag('news/list_categories.html')
+
+def show_categories(arg1='Последние', arg2='Новости'):
+    categories = Category.objects.all()
+    return {'categories': categories, 'arg1': arg1, 'arg2': arg2}
--- MySite/news/urls.py
+++ MySite/news/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from django.views.generic import detail
-
 from .views import *
 
+app_name = 'news'
+
 urlpatterns = [
-    path('', index),
-    path('test/', test),
+    path('', index, name='home'),
+    path('test/', test, name='test'),
     path('category/<int:category_id>/', get_category, name='category'),
 ]
