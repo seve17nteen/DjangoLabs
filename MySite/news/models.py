b360a2a1235e1975d0b3a828193191efd9fba0e1
seve17nteen <raydenonelove@gmail.com>
2026-06-01 05:19:43 +0300

    13 лабораторная: Django ORM, модели Author и Comment, запросы

--- MySite/news/models.py
+++ MySite/news/models.py
@@ -9,8 +9,7 @@ class Category(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        # Исправлено: добавлено пространство имен 'news:' и правильное имя маршрута
-        return reverse('news:category', kwargs={'category_id': self.pk})  # ← для категории
+        return reverse('news:category', kwargs={'category_id': self.pk})
 
 
 class News(models.Model):
@@ -20,11 +19,11 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, verbose_name='Категория', related_name='get_news')
+    views = models.IntegerField(default=0, verbose_name='Просмотры')
 
     def get_absolute_url(self):
-        # Исправлено: kwargs={'pk': self.pk} (т.к. в DetailView используется pk)
-        return reverse('news:view_news', kwargs={'pk': self.pk})  # ← для новости
+        return reverse('news:view_news', kwargs={'pk': self.pk})
 
     def my_func(self):
         return "Последние новости"
@@ -35,4 +34,38 @@ class News(models.Model):
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
-        ordering = ['created_at', ]
+        ordering = ['-created_at']  # сортировка от новых к старым
+
+
+#НОВЫЕ МОДЕЛИ ДЛЯ 13 ЛАБОРАТОРНОЙ
+
+class Author(models.Model):
+    """Модель автора комментариев"""
+    name = models.CharField(max_length=100, verbose_name='Имя автора')
+    email = models.EmailField(verbose_name='Email', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+        ordering = ['name']
+
+
+class Comment(models.Model):
+    """Модель комментария к новости"""
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='comments', verbose_name='Автор')
+    news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='comments', verbose_name='Новость')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies', verbose_name='Ответ на комментарий')
+    text = models.TextField(verbose_name='Текст комментария')
+    rating = models.IntegerField(default=0, verbose_name='Рейтинг')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return f'Комментарий от {self.author.name} к "{self.news.title[:30]}"'
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['-created_at']
--- MySite/news/templatetags/news_tags.py
+++ MySite/news/templatetags/news_tags.py
@@ -1,16 +1,14 @@
-print("news_tags.py загружен!")
-
-
 from django import template
+from django.db.models import Count
 from news.models import Category
-register=template.Library()
-@register.simple_tag(name='get_list_categories')
 
+register = template.Library()
+
+@register.simple_tag(name='get_list_categories')
 def get_categories():
     return Category.objects.all()
 
 @register.inclusion_tag('news/list_categories.html')
-
 def show_categories(arg1='Последние', arg2='Новости'):
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('get_news')).filter(cnt__gt=0)
     return {'categories': categories, 'arg1': arg1, 'arg2': arg2}
